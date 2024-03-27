810ec9830bbf1f0952928bfa44073f4fd9f14654
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-03-26 19:46:48 -0500

    Partial changes in content-title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { ContentTitleComponent } from './components/content-title/content-title.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ContentTitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
