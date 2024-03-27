658d5ff47f2e476ae5f4dab33a198607be5ae782
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-03-27 16:49:35 -0500

    targets content created

---
+++ src/app/components/targets/targets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-targets',
+  standalone: true,
+  imports: [],
+  templateUrl: './targets.component.html',
+  styleUrl: './targets.component.scss'
+})
+export class TargetsComponent {
+
+}
