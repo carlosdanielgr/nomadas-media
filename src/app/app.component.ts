150f4efae3029409a5e2fe0e7d47fb179c9df92c
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-03-27 16:49:11 -0500

    targets content completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { ContentTitleComponent } from './components/content-title/content-title.component';
+import { TargetsComponent } from './components/targets/targets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, ContentTitleComponent],
+  imports: [HeaderComponent, ContentTitleComponent, TargetsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
