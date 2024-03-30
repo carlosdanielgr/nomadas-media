3d44edffecc2e86d6a9fb093ac9d9602fb150c4b
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-03-29 19:46:55 -0500

    form component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { ServicesComponent } from './components/services/services.component';
 import { ContentInfoComponent } from './components/content-info/content-info.component';
 import { StrategyComponent } from './components/strategy/strategy.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { FormComponent } from './components/form/form.component';
 
 @Component({
   selector: 'app-root',
@@ -20,6 +21,7 @@ import { ProjectsComponent } from './components/projects/projects.component';
     ContentInfoComponent,
     StrategyComponent,
     ProjectsComponent,
+    FormComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [ReactiveFormsModule, NgbDropdownModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss',
+})
+export class FormComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(private readonly formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formInit();
+  }
+
+  private formInit() {
+    this.form = this.formBuilder.group({
+      full_name: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      phone: ['', [Validators.required]],
+      company_name: ['', [Validators.required]],
+      marketing: ['', [Validators.required]],
+      program: ['', [Validators.required]],
+      business: ['', [Validators.required]],
+    });
+  }
+
+  onSelectItem(value: string, control: string) {
+    this.form.get(control)?.setValue(value);
+  }
+}
